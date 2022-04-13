09d5f9f497aa7872f85017777b45ed2a76a6b105
rajan9873 <87296769+rajan9873@users.noreply.github.com>
2022-04-13 13:49:25 +0530

    new commit

---
+++ src/Component/Trading.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Trading = () => {
+  return (
+    <>
+      <div className="trading-main">
+        <div className="trading-section-a">
+          <div className="currency-details">
+            {/* <h1>₹{currentCurrency.rate.toFixed(2)}</h1> */}
+          </div>
+          <div className="graph"></div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Trading;
